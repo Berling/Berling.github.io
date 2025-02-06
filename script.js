6d79c998dab151a244bd2e83a7ee3c83a5a94361
Georg Schäfer <georg.peter.schaefer@gmail.com>
2025-02-06 23:36:29 +0100

    feat: add burger menu

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    var links = document.getElementById("links");
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
+}
+
+function closeMenu() {
+    document.getElementById("links").style.display = "none";
+}
