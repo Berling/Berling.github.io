bb26b459526bfad86db55c8dafd3f709d1ef1c29
Georg Schäfer <georg.peter.schaefer@gmail.com>
2025-02-07 17:47:13 +0100

    feat: make open menu modal

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+function preventDefault(e) {
+    e.preventDefault();
+}
+
 function toggleMenu() {
     var menu = document.getElementById("menu");
+    var modal = document.getElementById("modal");
     if (menu.style.display === "block") {
+        document.body.style.overflow = "unset";
         menu.style.display = "none";
+        modal.style.display = "none";
     } else {
+        document.body.style.overflow = "hidden";
         menu.style.display = "block";
+        modal.style.display = "block";
     }
 }
 
 function closeMenu() {
     document.getElementById("menu").style.display = "none";
+    document.getElementById("modal").style.display = "none";
 }
