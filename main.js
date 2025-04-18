a3b5687e7b659e17545a7df0ccf1f823ad5c7d0b
Georg Schäfer <georg.peter.schaefer@gmail.com>
2025-04-18 21:35:58 +0200

    refactor: move app content out of root component template

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import App from "./app/app.component.js"
+import HomeSection from "./app/home-section/home-section.component.js"
+import ContentSection from "./app/content-section/content-section.component.js"
+import EventList from "./app/event-list/event-list.component.js"
+import BandList from "./app/band-list/band-list.component.js"
+import DonationList from "./app/donation-list/donation-list.component.js"
