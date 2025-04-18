76175e0d6caead12639f32e8d583506de3e71d57
Georg Schäfer <georg.peter.schaefer@gmail.com>
2025-04-18 17:19:35 +0200

    feature: add events component

---
+++ app/event-list/event-list.component.js
@@ -0,0 +1,51 @@
+import defineComponent from "../../component.js"
+
+export default await defineComponent(
+    "event-list",
+    "./app/event-list/event-list.component.html",
+    "./app/event-list/event-list.component.css",
+    class EventList extends HTMLElement {
+        constructor() {
+            super();
+        }
+
+        async connectedCallback() {
+            let response = await fetch("content/events");
+            if (response.status != 200) {
+                return;
+            }
+            let text = await response.text();
+            let lines = text.split("\n");
+            let events = [];
+            for (let i = 0; i < lines.length; i++) {
+                let fields = lines[i].split(";");
+                let event = {
+                    "date": new Date(fields[0]),
+                    "bands": fields[1],
+                    "eventId": fields[2]
+                };
+                events.push(event);
+            }
+
+            events.sort((a, b) => b.date - a.date);
+            let today = new Date();
+            let upcomingEvents = events.filter((event) => event.date >= today).reverse();
+            let upcomingList = this.shadowRoot.getElementById("upcoming");
+            upcomingEvents.forEach(event => {
+                let upcomingEntry = upcomingList.appendChild(document.createElement("li"));
+                upcomingEntry.classList += "upcoming-entry";
+                let content = upcomingEntry.appendChild(document.createElement("a"));
+                content.href = "https://www.facebook.com/events/" + event.eventId;
+                content.textContent = event.date.toLocaleDateString() + " - " + event.bands;
+            });
+            let pastEvents = events.filter((event) => event.date < today);
+            let pastList = this.shadowRoot.getElementById("past");
+            pastEvents.forEach(event => {
+                let pastEntry = pastList.appendChild(document.createElement("li"));
+                pastEntry.classList += "past-entry";
+                let content = pastEntry.appendChild(document.createElement("a"));
+                content.href = "https://www.facebook.com/events/" + event.eventId;
+                content.textContent = event.date.toLocaleDateString() + " - " + event.bands;
+            });
+        }
+    });
