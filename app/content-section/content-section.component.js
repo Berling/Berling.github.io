8fadefdf3a3b94b3f45248b64b997a552c6e5c2e
Georg Schäfer <georg.peter.schaefer@gmail.com>
2025-04-19 11:02:06 +0200

    feature: add imprint page

--- app/content-section/content-section.component.js
+++ app/content-section/content-section.component.js
@@ -5,19 +5,16 @@ export default await defineComponent(
     "./app/content-section/content-section.component.html",
     "./app/content-section/content-section.component.css",
     class ContentSection extends HTMLElement {
-        static _alternateColorScheme = false;
-
         constructor() {
             super();
         }
 
         connectedCallback() {
             const content = this.shadowRoot.getElementById("content");
-            if (ContentSection._alternateColorScheme) {
+            if (this.hasAttribute("alt")) {
                 content.classList.add("alt");
             } else {
                 content.classList.remove("alt");
             }
-            ContentSection._alternateColorScheme = !ContentSection._alternateColorScheme;
         }
     });
