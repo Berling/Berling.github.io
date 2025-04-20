ec6758f038ccfa2c031586699d79ade715d09bb9
Georg Schäfer <georg.peter.schaefer@gmail.com>
2025-04-20 13:21:33 +0200

    feature: make menu modal when open

--- app/top-nav/top-nav.component.js
+++ app/top-nav/top-nav.component.js
@@ -18,21 +18,32 @@ export default await defineComponent(
             this.shadowRoot.querySelectorAll("#links>a").forEach(link => {
                 link.onclick = () => this.closeMenu();
             });
+
+            const modal = this.shadowRoot.getElementById("modal");
+            modal.onclick = () => this.closeMenu();
         }
 
         toggleMenu() {
             const menu = this.shadowRoot.getElementById("menu");
+            const modal = this.shadowRoot.getElementById("modal");
             if (this.open) {
                 menu.classList.remove("open");
+                modal.classList.remove("active");
+                document.body.style.overflow = "unset";
             } else {
                 menu.classList.add("open");
+                modal.classList.add("active");
+                document.body.style.overflow = "hidden";
             }
             this.open = !this.open;
         }
 
         closeMenu() {
             const menu = this.shadowRoot.getElementById("menu");
+            const modal = this.shadowRoot.getElementById("modal");
             menu.classList.remove("open");
+            modal.classList.remove("active");
+            document.body.style.overflow = "unset";
             this.open = false;
         }
     });
